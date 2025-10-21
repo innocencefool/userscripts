3ee55c0081afb4987d0b87ad31e308ddbc55626b
小古 <innocence.fool@outlook.com>
2025-10-21 16:29:56 +0800

    Update itdog.js

--- itdog.js
+++ itdog.js
@@ -1,3 +1,20 @@
+// ==UserScript==
+// @name         ITDOG
+// @namespace    http://tampermonkey.net/
+// @version      2024-11-14
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.itdog.cn/ping/*
+// @match        https://www.itdog.cn/tcping/*
+// @match        https://www.itdog.cn/ping_ipv6/*
+// @match        https://www.itdog.cn/tcping_ipv6/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
 const table = document.querySelector('#simpletable');
 if (table != null) {
     table.querySelector('thead').style.display = 'flex';
@@ -38,27 +55,43 @@ if (table != null) {
             tds.forEach((e, i) => {
                 if (i == 0) {
                     if (e.querySelector('span').textContent == '电信') {
-                        let order = 1000000;
+                        let order = 10000000;
                         for (let i = 3; i <= 4; i++) {
-                            order += e.innerText.charCodeAt(i) * 10;
+                            if (i == 3) {
+                                order += e.innerText.charCodeAt(i) * 100;
+                            } else {
+                                order += e.innerText.charCodeAt(i) * 10;
+                            }
                         }
                         tr.style.order = order + e.innerText.charCodeAt(e.innerText.length - 1);
                     } else if (e.querySelector('span').textContent == '联通') {
-                        let order = 2000000;
+                        let order = 20000000;
                         for (let i = 3; i <= 4; i++) {
-                            order += e.innerText.charCodeAt(i) * 10;
+                            if (i == 3) {
+                                order += e.innerText.charCodeAt(i) * 100;
+                            } else {
+                                order += e.innerText.charCodeAt(i) * 10;
+                            }
                         }
                         tr.style.order = order + e.innerText.charCodeAt(e.innerText.length - 1);
                     } else if (e.querySelector('span').textContent == '移动') {
-                        let order = 3000000;
+                        let order = 30000000;
                         for (let i = 3; i <= 4; i++) {
-                            order += e.innerText.charCodeAt(i) * 10;
+                            if (i == 3) {
+                                order += e.innerText.charCodeAt(i) * 100;
+                            } else {
+                                order += e.innerText.charCodeAt(i) * 10;
+                            }
                         }
                         tr.style.order = order + e.innerText.charCodeAt(e.innerText.length - 1);
                     } else {
-                        let order = 4000000;
+                        let order = 40000000;
                         for (let i = 3; i <= 4; i++) {
-                            order += e.innerText.charCodeAt(i) * 10;
+                            if (i == 3) {
+                                order += e.innerText.charCodeAt(i) * 100;
+                            } else {
+                                order += e.innerText.charCodeAt(i) * 10;
+                            }
                         }
                         tr.style.order = order + e.innerText.charCodeAt(e.innerText.length - 1);
                     }
@@ -74,27 +107,43 @@ if (table != null) {
             tds.forEach((e, i) => {
                 if (i == 0) {
                     if (e.querySelector('span').textContent == '电信') {
-                        let order = 1000000;
+                        let order = 10000000;
                         for (let i = 3; i <= 4; i++) {
-                            order += e.innerText.charCodeAt(i) * 10;
+                            if (i == 3) {
+                                order += e.innerText.charCodeAt(i) * 100;
+                            } else {
+                                order += e.innerText.charCodeAt(i) * 10;
+                            }
                         }
                         tr.style.order = order + e.innerText.charCodeAt(e.innerText.length - 1);
                     } else if (e.querySelector('span').textContent == '联通') {
-                        let order = 2000000;
+                        let order = 20000000;
                         for (let i = 3; i <= 4; i++) {
-                            order += e.innerText.charCodeAt(i) * 10;
+                            if (i == 3) {
+                                order += e.innerText.charCodeAt(i) * 100;
+                            } else {
+                                order += e.innerText.charCodeAt(i) * 10;
+                            }
                         }
                         tr.style.order = order + e.innerText.charCodeAt(e.innerText.length - 1);
                     } else if (e.querySelector('span').textContent == '移动') {
-                        let order = 3000000;
+                        let order = 30000000;
                         for (let i = 3; i <= 4; i++) {
-                            order += e.innerText.charCodeAt(i) * 10;
+                            if (i == 3) {
+                                order += e.innerText.charCodeAt(i) * 100;
+                            } else {
+                                order += e.innerText.charCodeAt(i) * 10;
+                            }
                         }
                         tr.style.order = order + e.innerText.charCodeAt(e.innerText.length - 1);
                     } else {
-                        let order = 4000000;
+                        let order = 40000000;
                         for (let i = 3; i <= 4; i++) {
-                            order += e.innerText.charCodeAt(i) * 10;
+                            if (i == 3) {
+                                order += e.innerText.charCodeAt(i) * 100;
+                            } else {
+                                order += e.innerText.charCodeAt(i) * 10;
+                            }
                         }
                         tr.style.order = order + e.innerText.charCodeAt(e.innerText.length - 1);
                     }
@@ -115,3 +164,5 @@ if (table != null) {
         }
     });
 }
+
+})();
